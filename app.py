fbef34333569b5ba683ebedde6ccad3df5910e5c
Samuel Valdez <savaldez@csumb.edu>
2017-03-29 06:11:06 +0000

    wrote the readme

--- app.py
+++ app.py
@@ -10,6 +10,17 @@ import random
 
 
 
+send_url = 'http://freegeoip.net/json'
+r = requests.get(send_url)
+j = json.loads(r.text)
+lat = j['latitude']
+lon = j['longitude']
+
+print lat
+print lon
+
+
+
 app = flask.Flask(__name__)
 socketio = flask_socketio.SocketIO(app)
 
