cdb03549e9aea55ee108c1ca381c78ecfd5e9f39
Maria Loza <mloza@csumb.edu>
2017-04-17 07:12:12 +0000

    added fb login/logout

--- scripts/Main.js
+++ scripts/Main.js
@@ -2,5 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import { Content } from './Content';
+import { Facebook } from './Facebook';
 
 ReactDOM.render(<Content />, document.getElementById('content'));
+ReactDOM.render(<Facebook />, document.getElementById('facebook'));
--- static/script.js
+++ static/script.js
@@ -13320,9 +13320,12 @@ var ReactDOM = _interopRequireWildcard(_reactDom);
 
 var _Content = __webpack_require__(105);
 
+var _Facebook = __webpack_require__(236);
+
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 ReactDOM.render(React.createElement(_Content.Content, null), document.getElementById('content'));
+ReactDOM.render(React.createElement(_Facebook.Facebook, null), document.getElementById('facebook'));
 
 /***/ }),
 /* 111 */
@@ -30489,6 +30492,70 @@ function toArray(list, index) {
 /***/ (function(module, exports) {
 
 /* (ignored) */
+
+/***/ }),
+/* 236 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.Facebook = undefined;
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(60);
+
+var React = _interopRequireWildcard(_react);
+
+var _Socket = __webpack_require__(111);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Facebook = exports.Facebook = function (_React$Component) {
+    _inherits(Facebook, _React$Component);
+
+    function Facebook(props) {
+        _classCallCheck(this, Facebook);
+
+        var _this = _possibleConstructorReturn(this, (Facebook.__proto__ || Object.getPrototypeOf(Facebook)).call(this, props));
+
+        _this.state = {};
+
+        return _this;
+    }
+
+    _createClass(Facebook, [{
+        key: 'componentDidMount',
+        value: function componentDidMount() {}
+    }, {
+        key: 'render',
+        value: function render() {
+
+            return React.createElement(
+                'div',
+                null,
+                React.createElement('div', {
+                    className: 'fb-login-button',
+                    'data-max-rows': '1',
+                    'data-size': 'medium',
+                    'data-show-faces': 'false',
+                    'data-auto-logout-link': 'true' })
+            );
+        }
+    }]);
+
+    return Facebook;
+}(React.Component);
 
 /***/ })
 /******/ ]);
